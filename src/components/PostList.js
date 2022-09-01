c3de68ca8d8961c73d4d36f0bc084e054dc04bd9
jinist94 <325days@naver.com>
2022-09-01 12:42:27 +0900

    fix: 오류 코드 수정

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,12 +1,12 @@
-import { push } from "../util/router.js";
-import { addOpenListId, getItem, OPEN_LIST, removeOpenListId, SELECTED_DOCUMENT, setItem } from "../util/storage.js";
-import { compareName, findDocumentId } from "../util/util.js";
-import { createElement } from "../util/helper.js";
+import { push } from '../util/router.js';
+import { addOpenListId, getItem, OPEN_LIST, removeOpenListId, SELECTED_DOCUMENT, setItem } from '../util/storage.js';
+import { compareName, findDocumentId } from '../util/util.js';
+import { createElement } from '../util/helper.js';
 
 export default function PostList({ $target, initialState, onAdd, onRemove, onSelect }) {
   this.state = initialState;
 
-  const $document = createElement("ul");
+  const $document = createElement('ul');
   $target.appendChild($document);
 
   let openList = getItem(OPEN_LIST, null);
@@ -22,30 +22,29 @@ export default function PostList({ $target, initialState, onAdd, onRemove, onSel
   };
 
   this.render = () => {
-    $document.innerHTML = "";
+    $document.innerHTML = '';
     $document.innerHTML = createPost(this.state, 1);
   };
 
   const toggleBtn = (isOpen) => {
-    return `<button class="item__button--toggle"> ${isOpen ? "▼" : "▶"}</button>`;
-  }
+    return `<button class="item__button--toggle"> ${isOpen ? '▼' : '▶'}</button>`;
+  };
 
   const AddDocumentBtn = () => {
     return '<button class="item__button--add"><i class="fa-solid fa-plus"></i></button>';
-  }
+  };
 
   const childDocuments = (documents, depth) => {
-    if(documents.length > 0){
+    if (documents.length > 0) {
       return `<ul>${createPost(documents, depth + 1)}</ul>`;
-    } 
-    
-    return  `<ul><li class="empty-post-message"> <span>하위 페이지가 없습니다</span></li> <ul>`;
-  }
+    }
 
+    return `<ul><li class="empty-post-message"> <span>하위 페이지가 없습니다</span></li> <ul>`;
+  };
 
   const createPost = (documents, depth) => {
     const MAX_DEPTH = 5;
-    const POST_PADDING = depth * 10;Template literal
+    const POST_PADDING = depth * 10;
 
     const selected = getItem(SELECTED_DOCUMENT, null);
 
@@ -54,51 +53,51 @@ export default function PostList({ $target, initialState, onAdd, onRemove, onSel
       .map(({ id, title, documents }) => {
         const isOpen = (openList && openList[id]) || false;
         const paddingLeft = depth === MAX_DEPTH ? POST_PADDING + 10 : POST_PADDING;
-        const isSelected = selected && selected.id == id ? "selected" : "";
- 
+        const isSelected = selected && selected.id == id ? 'selected' : '';
+
         return `
           <li class="post__item" data-id="${id}">
             <div class="item__content ${isSelected}" style="padding-left:${paddingLeft}px">
-              ${depth < MAX_DEPTH ? toggleBtn(isOpen) : ""}
-              <div class="item__title">${title ? title : "제목 없음"}</div>
+              ${depth < MAX_DEPTH ? toggleBtn(isOpen) : ''}
+              <div class="item__title">${title ? title : '제목 없음'}</div>
               <div class="item__buttons">
                 <button class="item__button--remove"><i class="fa-solid fa-trash-can"></i> </button>
                 ${depth < MAX_DEPTH ? AddDocumentBtn() : ''}
               </div>
             </div>
-            ${isOpen ? childDocuments(documents, depth + 1) : "" }
+            ${isOpen ? childDocuments(documents, depth + 1) : ''}
           </li>
          `;
       })
-      .join("")}
+      .join('')}
     `;
   };
 
   this.render();
 
-  $document.addEventListener("click", async (e) => {
-    const clickElement = compareName(e.target.tagName, "I") ? e.target.closest("button") : e.target;
-    const $li = clickElement.closest(".post__item");
+  $document.addEventListener('click', async (e) => {
+    const clickElement = compareName(e.target.tagName, 'I') ? e.target.closest('button') : e.target;
+    const $li = clickElement.closest('.post__item');
 
     if ($li) {
       const documentId = $li.dataset.id;
 
       switch (clickElement.className) {
-        case "item__button--remove":
+        case 'item__button--remove':
           removeOpenListId(documentId);
           const paramId = findDocumentId();
 
-          if (documentId === paramId) push("/");
+          if (documentId === paramId) push('/');
 
           onRemove(documentId);
           break;
 
-        case "item__button--add":
+        case 'item__button--add':
           addOpenListId(documentId);
           onAdd(documentId);
           break;
 
-        case "item__button--toggle":
+        case 'item__button--toggle':
           openList && openList[documentId] ? removeOpenListId(documentId) : addOpenListId(documentId);
           this.setState();
           break;
